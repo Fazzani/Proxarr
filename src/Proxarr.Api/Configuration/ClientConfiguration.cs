3c3f424cb4b130d611c225a2b55c26a53a0d0574
Fazzani <heni.fazzani@gmail.com>
2024-12-29 00:07:50 +0100

    Some fixes

--- src/Proxarr.Api/Configuration/ClientConfiguration.cs
+++ src/Proxarr.Api/Configuration/ClientConfiguration.cs
@@ -3,7 +3,16 @@
     public sealed class ClientConfiguration
     {
         public const string SECTION_NAME = $"{AppConfiguration.SECTION_NAME}:Clients";
+
+        /// <summary>
+        /// Must be Sonarr or Radarr
+        /// </summary>
+        public required string Application { get; set; }
         public required string ApiKey { get; set; }
         public required string BaseUrl { get; set; }
+
+
+        public bool IsSonarr => Application.Equals("Sonarr", StringComparison.OrdinalIgnoreCase);
+        public bool IsRadarr => Application.Equals("Radarr", StringComparison.OrdinalIgnoreCase);
     }
 }
