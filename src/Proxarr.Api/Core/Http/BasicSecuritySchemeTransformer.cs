63c0f3fbe9ec2314e905415b73307be9f474b6d5
Fazzani <heni.fazzani@gmail.com>
2024-12-31 17:10:24 +0100

    feat: Add basic auth to scalar and versioning assembly

---
+++ src/Proxarr.Api/Core/Http/BasicSecuritySchemeTransformer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Proxarr.Api.Core.Http
+{
+    [ExcludeFromCodeCoverage]
+    internal sealed class BasicSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
+    {
+        public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+        {
+            var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+            if (authenticationSchemes.Any(authScheme => authScheme.Name == BasicAuthenticationDefaults.AuthenticationScheme))
+            {
+                var requirements = new Dictionary<string, OpenApiSecurityScheme>
+                {
+                    ["Basic"] = new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.Http,
+                        Scheme = BasicAuthenticationDefaults.AuthenticationScheme,
+                        In = ParameterLocation.Header,
+                    }
+                };
+                document.Components ??= new OpenApiComponents();
+                document.Components.SecuritySchemes = requirements;
+
+                // Apply it as a requirement for all operations
+                foreach (var operation in document.Paths.Values.SelectMany(path => path.Operations))
+                {
+                    operation.Value.Security.Add(new OpenApiSecurityRequirement
+                    {
+                        [new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = BasicAuthenticationDefaults.AuthenticationScheme,
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        }] = Array.Empty<string>()
+                    });
+                }
+            }
+        }
+    }
+}
